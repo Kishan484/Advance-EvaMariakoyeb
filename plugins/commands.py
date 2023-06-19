e2c139199b5ef86964794ff0dae472e03ec65af0
Kishan484 <121420677+Kishan484@users.noreply.github.com>
2023-06-19 15:55:39 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -244,6 +244,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='https://t.me/Flix_Movie_in') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
